f0206f608b59f7bf86ce6741b3971e3102481d55
onlyrenoy <onlyrenoy@gmail.com>
2019-09-12 20:41:44 +0200

    Add files via upload

---
+++ Genius_Project/Genius_Project/C/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Genius_Project
+//
+//  Created by admin on 04/09/2019.
+//  Copyright © 2019 RenoyChowdhury. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var getAccountToken = ""
+    var urlWithAT = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.navigationDelegate = self
+        let authURL = String(format: "%@?client_id=%@&redirect_uri=%@&response_type=token", arguments: [API.INSTAGRAM_AUTHURL,API.INSTAGRAM_CLIENT_ID,API.INSTAGRAM_REDIRECT_URI])
+        let urlRequest = URLRequest.init(url: URL.init(string: authURL)!)
+        webView.load(urlRequest)
+    }
+}
+
+extension ViewController: WKNavigationDelegate  {
+    //URL TO GET INFO URL(string: "https://api.instagram.com/v1/users/self/?access_token=\(getAccountToken)")
+    //URL TO GET IMAGES URL(string: "https://api.instagram.com/v1/users/self/media/recent/?access_token=\(getAccountToken)")
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let accountTokenFromURL = webView.url?.absoluteString else { return }
+        guard let getToken = accountTokenFromURL.components(separatedBy: "#access_token=").last else{
+            return
+        }
+        
+        if accountTokenFromURL.contains("access_token") {
+            getAccountToken = getToken
+            
+            if let AT = URL(string: "https://api.instagram.com/v1/users/self/?access_token=\(getAccountToken)"){
+                let session = URLSession.shared
+                let task = session.dataTask(with: AT) { (data, resp, err) in
+                    if let data = data {
+                        if let json = try? JSONSerialization.jsonObject(with: data, options: []) {
+                            print(json)
+                        }
+                    }
+                }
+                task.resume()
+            }
+        }
+        decisionHandler(.allow)
+    }
+}
---
+++ Genius_Project/Genius_Project/M/API.swift
@@ -0,0 +1,20 @@
+//
+//  API.swift
+//  Genius_Project
+//
+//  Created by admin on 07/09/2019.
+//  Copyright © 2019 RenoyChowdhury. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+    static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
+    static let INSTAGRAM_CLIENT_ID = "2a9897bcff4b459ea0a47680b9032756"
+    static let INSTAGRAM_CLIENTSERCRET = "df03b16577954349b6ac515efec7a96d"
+    static let INSTAGRAM_REDIRECT_URI = "https://localhost"
+    static let INSTAGRAM_ACCESS_TOKEN = "access"
+    static let INSTAGRAM_SCOPE = "follower_list"
+    
+    static let IG_LOGGED = "https://api.instagram.com/v1/users/self/"
+}
